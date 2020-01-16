2c95a2a0fcaa19e4e65252da3b233466a3eee472
streamlineracing <55709121+streamlineracing@users.noreply.github.com>
2020-01-15 22:46:53 -0500

    added gif

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ var date = new Date();
 
 var yearLabel = document.getElementById("countdown");
 yearLabel.innerHTML = targetDate.getFullYear() - date.getFullYear();
+
+var canvas = document.getElementById("myCanvas")
+var ctx = canvas.getContext("2d");
+var img = document.createElement("img");
+img.setAttribute('src' , "https://www.formula1.com/content/fom-website/en/teams/Renault/_jcr_content/teamCar.img.jpg");
+
+
+ctx.drawImage(img, 100,500, 150, 75)
+
+var x = 100
+
+function driveCar(){
+  x+=100;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img,x,500,200,100);
+
+  if(x>1750){
+    clearInterval(myInt);
+    canvas.style.visibility = "hidden";
+  }
+    
+}
+
+var myInt = setInterval(driveCar, 50)
+
